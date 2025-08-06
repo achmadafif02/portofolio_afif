5ac040ac24e717c4f12b7948807a144e5212024a
achmadafif02 <achmadafif2007@gmail.com>
2025-08-06 07:41:40 +0700

    upload file web portofolio

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const text = "Hallo , Saya Achmad Afif";
+const typingElement = document.getElementById("typing-text");
+
+let charIndex = 0;
+let isDeleting = false;
+
+function typeLoop() {
+  if (!isDeleting) {
+    typingElement.textContent = text.substring(0, charIndex + 1);
+    charIndex++;
+
+    if (charIndex === text.length) {
+      isDeleting = true;
+      setTimeout(typeLoop, 2500); // jeda sebelum menghapus
+      return;
+    }
+  } else {
+    typingElement.textContent = text.substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isDeleting = false;
+    }
+  }
+
+  setTimeout(typeLoop, isDeleting ? 100 : 200); // kecepatan mengetik / hapus
+}
+
+document.addEventListener("DOMContentLoaded", typeLoop);
